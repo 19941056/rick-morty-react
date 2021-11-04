2cb1dfa7888025e4e8fe54dde61527efe1b2d441
Antonio Maestre Vargas <antoniomaestrevargasgmail.com@MBP-de-Antonio.home>
2021-11-04 02:48:51 +0100

    paint components

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import './App.css';
+import React from 'react'
+import { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [characterName, setCharacterName] = useState("");
+  const [characterChosen,setCharacterChosen]=useState(false);
+  const [character, setCharacter] = useState({
+    name:'',
+    id:'',
+    image:'',
+  });
+
+
+  const searchCharacter=()=>{
+    axios.get('https://rickandmortyapi.com/api/character/?name=' +characterName).then(
+      (res) => {
+        setCharacter({
+          name:characterName,
+          id:res.data.results[0].id,
+          image:res.data.results[2].image,
+        });
+        setCharacterChosen(true);
+      }
+    );
+  };
+
+  return (
+    <div className="App">
+    <div className='TitleSection'>
+    <h1>Rick and Morty</h1>
+    <input type='text' onChange={(event)=>{
+      setCharacterName(event.target.value);
+    }}></input>
+    <button onClick={searchCharacter}>Search Character</button>
+    </div>
+    <div className="DisplaySection">
+    {!characterChosen ? (
+          <h1> Please choose a Character </h1>
+        ) : (
+          <>
+            <h2>{character.name}</h2>
+            <img src={character.image} alt={character.name} />
+            <p>#{character.id}</p>
+          </>
+          
+        )}
+    </div>
+      
+    </div>
+  )
+}
+
+export default App
